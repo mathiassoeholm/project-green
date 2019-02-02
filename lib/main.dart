054eb14673ca9f52f94362d938e537289ec632a0
Mathias Soeholm <mathiassoeholm@gmail.com>
2019-02-03 00:09:58 +0100

    Made a list with some fake data

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,99 @@
+import 'package:built_collection/built_collection.dart';
 import 'package:flutter/material.dart';
+import 'package:project_green/appstate/app_state.dart';
+import 'package:project_green/appstate/app_state_reducer.dart';
+import 'package:project_green/challenges/challenge.dart';
 import 'package:project_green/localization/app_localizations.dart';
 import 'package:project_green/widgets/home.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:redux/redux.dart';
 
 void main() {
-  runApp(MaterialApp(
-    localizationsDelegates: [
-      AppLocalizations.delegate,
-      GlobalMaterialLocalizations.delegate,
-      GlobalWidgetsLocalizations.delegate,
-    ],
-    supportedLocales: [
-      const Locale('en', 'US'),
-      const Locale('da', 'DK'),
-    ],
-    home: Home(),
+
+  final initialState = AppState((b) => b
+    ..challenges = BuiltList<Challenge>([
+      Challenge((b) => b
+        ..emoji = "🐯"
+      ),
+      Challenge((b) => b
+        ..emoji = "🐯"
+      ),
+      Challenge((b) => b
+        ..emoji = "🐯"
+      ),
+      Challenge((b) => b
+        ..emoji = "🐯"
+      ),
+      Challenge((b) => b
+        ..emoji = "🐯"
+      ),
+      Challenge((b) => b
+        ..emoji = "🐯"
+      ),
+      Challenge((b) => b
+        ..emoji = "🐯"
+      ),
+      Challenge((b) => b
+        ..emoji = "🐯"
+      ),
+      Challenge((b) => b
+        ..emoji = "🐯"
+      ),
+      Challenge((b) => b
+        ..emoji = "🐯"
+      ),
+      Challenge((b) => b
+        ..emoji = "🐯"
+      ),
+      Challenge((b) => b
+        ..emoji = "🐯"
+      ),
+      Challenge((b) => b
+        ..emoji = "🐯"
+      ),
+      Challenge((b) => b
+        ..emoji = "🐯"
+      ),
+      Challenge((b) => b
+        ..emoji = "🐯"
+      ),
+      Challenge((b) => b
+        ..emoji = "🐯"
+      ),
+      Challenge((b) => b
+        ..emoji = "🐯"
+      ),
+      Challenge((b) => b
+        ..emoji = "🐯"
+      ),
+      Challenge((b) => b
+        ..emoji = "🐯"
+      ),
+    ]).toBuilder(),
+  );
+
+  final Store<AppState> store = Store<AppState>(
+      appStateReducer,
+      initialState: initialState,
+  );
+
+  runApp(StoreProvider(
+    store: store,
+    child: MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      localizationsDelegates: [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('en', 'US'),
+        const Locale('da', 'DK'),
+      ],
+      home: Home(),
+    ),
   ));
 }
--- lib/widgets/home.dart
+++ lib/widgets/home.dart
@@ -1,11 +1,54 @@
+import 'package:built_collection/built_collection.dart';
 import 'package:flutter/material.dart';
-import 'package:project_green/localization/app_localizations.dart';
+import 'package:project_green/appstate/app_state.dart';
+import 'package:project_green/challenges/challenge.dart';
+import 'package:redux/redux.dart';
+import 'package:flutter_redux/flutter_redux.dart';
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text(AppLocalizations.of(context).title),
+    return StoreConnector<AppState, _ViewModel>(
+      converter: _ViewModel.fromStore,
+      builder: (BuildContext context, _ViewModel vm) {
+        return CustomScrollView(
+          slivers: <Widget>[
+            SliverAppBar(
+              expandedHeight: 100.0,
+              title: Text("my-challenges"),
+            ),
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (context, index) {
+                  return Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(vm.challenges[index].emoji,
+                        style: Theme.of(context).textTheme.title,
+                      ),
+                    ),
+                  );
+                },
+                childCount: vm.challenges.length,
+              )
+            )
+          ],
+        );
+      },
+    );
+  }
+}
+
+class _ViewModel {
+  final BuiltList<Challenge> challenges;
+
+  _ViewModel({
+    @required this.challenges,
+  });
+
+  static _ViewModel fromStore(Store<AppState> store) {
+    return _ViewModel(
+        challenges: store.state.challenges,
     );
   }
 }
